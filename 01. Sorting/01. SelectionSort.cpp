642ee2b40a9c7ec5367a40a13964021fdfda475d
Snehan Maity <snehanmaity@gmail.com>
2026-04-07 23:53:20 +0530

    Fix selection sort comparison logic

--- 01. Sorting/01. SelectionSort.cpp
+++ 01. Sorting/01. SelectionSort.cpp
@@ -10,7 +10,7 @@ void selectionSort(vector<int> &arr, int n){
 	for(int i =0;i<n-1;i++){
 		int mini=i;
 		for(int j=i+1;j<n;j++){
-			if(arr[j]<arr[i])	mini=j;
+			if(arr[j]<arr[mini])	mini=j;
 		}
 		swap(arr[i],arr[mini]);
 	}
