e2e6f36e057968e38b2240d85b0cfbeb1aec023e
Snehan Maity <snehanmaity@gmail.com>
2026-04-11 23:16:09 +0530

    Add files via upload

--- 04. BinarySearchOnAnswers/12. MedianOfTwoSortedArrays.cpp
+++ 04. BinarySearchOnAnswers/12. MedianOfTwoSortedArrays.cpp
@@ -20,7 +20,7 @@ public:
 			if(mid2-1 >= 0) 	l2 = b[mid2-1];
 			if(l1 <= r2 && l2 <= r1){
 				if(n%2 == 1) 	return max(l1, l2); 	// Odd
-				else return ((double) (max(l1,l2) + min(r1,r2)) / 2.0); 	// Even
+				else return ((double) (max(l1,l2) + (double)min(r1,r2)) / 2.0); 	// Even
 			}
 			else if(l1 > r2) 	high=mid1-1;
 			else 	low=mid1+1;
